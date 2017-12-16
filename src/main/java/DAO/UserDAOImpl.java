babeed36affc9fa738c3bdc5c96a1d62e2f6480c
zhoumengjiao <zhoumengjiao@zhoumengjiaodeMacBook-Air.local>
2017-12-15 20:58:55 -0500

    Merge remote-tracking branch 'remotes/origin/master'
    # Conflicts:
    #	src/main/webapp/static/JS/angular.js

--- src/main/java/DAO/UserDAOImpl.java
+++ src/main/java/DAO/UserDAOImpl.java
@@ -49,7 +49,8 @@ public class UserDAOImpl implements UserDAO {
         session = this.sessionFactory_UserDAO.getCurrentSession();
         if (!session.isOpen()) session = sessionFactory_UserDAO.openSession();
         Transaction tran = session.beginTransaction();
-        Query query = session.createQuery("from User user where user.UserName = " + UserName + " and user.PassWord = " + PassWord);
+    //    Query query = session.createQuery("from User user where user.UserName = " + UserName + " and user.PassWord = " + PassWord);
+        Query query = session.createQuery("from User user where user.UserName = 'wo' and user.PassWord ='123'");
         System.out.print(query.toString());
         List<User> users = query.list();
         tran.commit();
